c22ca47474c581032379c04f3b8585dd4f400679
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-03-31 00:58:23 -0700

    maps working with location getting data from geojson

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,54 +4,45 @@ import { ZoomControl } from 'mapbox-gl-controls';
 import { GeolocateControl } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'mapbox-gl-controls/lib/controls.css';
-import axios from 'axios';
+import geoJson from '../../data/places.json';
 import './Map.scss';
 
 function Map() {
-     const mapContainer = useRef([]);
-     const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+   const mapContainer = useRef([]);
+   const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-     const mapRef = useRef({});
+   const mapRef = useRef({});
 
-     useEffect(() => {
-          mapboxgl.accessToken = accessToken;
+   useEffect(() => {
+      mapboxgl.accessToken = accessToken;
 
-          // set map style and original location
-          mapRef.current = new mapboxgl.Map({
-               container: mapContainer.current,
-               style: 'mapbox://styles/mapbox/light-v10',
-               center: [-123.1607, 49.269],
-               zoom: 14,
-          });
+      // set map style and original location
+      mapRef.current = new mapboxgl.Map({
+         container: mapContainer.current,
+         style: 'mapbox://styles/mapbox/light-v10',
+         center: [-123.1607, 49.269],
+         zoom: 14,
+      });
 
-          // get current location
-          const geolocate = new GeolocateControl({
-               positionOptions: { enableHighAccuracy: true },
-               trackUserLocation: true,
-          });
+      // get current location
+      const geolocate = new GeolocateControl({
+         positionOptions: { enableHighAccuracy: true },
+         trackUserLocation: true,
+      });
 
-          mapRef.current.addControl(geolocate);
-          geolocate.on('geolocate', function (event) {
-               console.log(event.coords.latitude, event.coords.longitude);
-          });
+      mapRef.current.addControl(geolocate);
+      geolocate.on('geolocate', function (event) {
+         console.log(event.coords.latitude, event.coords.longitude);
+      });
 
-          // zoom control
-          mapRef.current.addControl(new ZoomControl(), 'bottom-right');
+      // zoom control
+      mapRef.current.addControl(new ZoomControl(), 'bottom-right');
 
-          axios.get('../data/data.geojson')
-               .then((response) => response.data)
-               .then((data) => {
-                    mapRef.current.addSource('markers', {
-                         type: 'geojson',
-                         data: data,
-                    });
-               })
-               .catch((error) => {
-                    console.log(error);
-               });
-     });
+      // Getting markers from GeoJson
+      geoJson.features.map((feature) => new mapboxgl.Marker().setLngLat(feature.geometry.coordinates).addTo(mapRef.current));
+   });
 
-     return <div className='map' ref={mapContainer}></div>;
+   return <div className='map' ref={mapContainer}></div>;
 }
 
 export default Map;

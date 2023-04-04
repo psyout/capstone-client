c770a9b4e1e964b2ac25779100090b550d5f9f5c
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-04-04 15:40:49 -0700

    create more funcionality for mapbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.scss';
-import Header from './components/Header/Header';
+import Map from './components/Map/Map';
 
 function App() {
    return (
       <div className='App'>
-         <Header />
+         <Map />
       </div>
    );
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,6 +6,7 @@ import { GeolocateControl } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'mapbox-gl-controls/lib/controls.css';
 import './Map.scss';
+// import geoJson from '../../data/places.json';
 
 export const apiUrl = 'http://localhost:3001';
 
@@ -41,33 +42,45 @@ function Map() {
       // zoom control
       mapRef.current.addControl(new ZoomControl(), 'bottom-right');
 
-      // fetch restaurants data from the server
+      // fetch restaurants data from the server\
+      let endpoints = [
+         `${apiUrl}/api/restaurants`,
+         `${apiUrl}/api/bars`,
+         `${apiUrl}/api/canadian`,
+         `${apiUrl}/api/seafood`,
+         `${apiUrl}/api/karaoke`,
+         `${apiUrl}/api/delis`,
+         `${apiUrl}/api/cideries`,
+         `${apiUrl}/api/mexican`,
+      ];
+
       axios
-         .get(`${apiUrl}/api/restaurants`)
-         .then((response) => {
-            setRestaurants(response.data.businesses);
+         .all(endpoints.map((endpoint) => axios.get(endpoint)))
+         .then(
+            axios.spread((...responses) => {
+               const businesses = responses.map((response) => response.data.businesses).flat();
+               setRestaurants(businesses);
+               businesses.forEach((business) => {
+                  const popup = new mapboxgl.Popup().setHTML(`
+                  <div>
+                    ${business.name}
+                    ${business.location.address1}
+                    ${business.location.city}, ${business.location.state} ${business.location.zip_code}
+                    Rating: ${business.rating} | Reviews: ${business.review_count}
+                  </div>`);
 
-            // Getting markers from restaurants data
-            response.data.businesses.forEach((business) => {
-               const popup = new mapboxgl.Popup().setHTML(`
-            <div>
-              ${business.name}
-              ${business.location.address1}
-              ${business.location.city}, ${business.location.state} ${business.location.zip_code}
-              Rating: ${business.rating} | Reviews: ${business.review_count}
-            </div>`);
+                  const marker = new mapboxgl.Marker()
+                     .setLngLat([business.coordinates.longitude, business.coordinates.latitude])
+                     .setPopup(popup)
+                     .addTo(mapRef.current);
 
-               const marker = new mapboxgl.Marker()
-                  .setLngLat([business.coordinates.longitude, business.coordinates.latitude])
-                  .setPopup(popup)
-                  .addTo(mapRef.current);
-
-               marker.getElement().addEventListener('click', function () {
-                  console.log(business);
-                  marker.togglePopup();
+                  marker.getElement().addEventListener('click', function () {
+                     console.log(business);
+                     marker.togglePopup();
+                  });
                });
-            });
-         })
+            })
+         )
          .catch((error) => {
             console.log(error);
          });
@@ -75,9 +88,9 @@ function Map() {
 
    return (
       <div className='map' ref={mapContainer}>
-         {restaurants.map((restaurant) => (
+         {/* {restaurants.map((restaurant) => (
             <div key={restaurant.id}>{restaurant.name}</div>
-         ))}
+         ))} */}
       </div>
    );
 }

c0c28604be3d14c84c64ceddd92689b1d4757a98
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-04-07 20:45:23 -0700

    finally manage to pass the data to each card when onclick each marker and add it first in the array of cards

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,21 +1,39 @@
 import './Aside.scss';
-import logo from '../../assets/images/logo.svg';
+import placeHolder from '../../assets/images/placeholder.jpg';
 import Card from '../Card/Card';
 
-function Aside({ selectedBusiness }) {
+function formatHours(hours) {
+   return Object.entries(hours).map(([key, value]) => `${key}: ${value}`);
+}
+
+function Aside({ selectedBusiness, geoJson }) {
+   const cards = geoJson.features.map((feature) => {
+      const { id, name, address, contact_number, website } = feature.properties;
+      const hours = feature.properties.hours.flatMap(formatHours);
+      return (
+         <Card
+            key={id}
+            title={name.slice(0, 25)}
+            subtitle={address}
+            number={contact_number}
+            imageSrc={placeHolder}
+            caption={hours}
+            website={website}
+         />
+      );
+   });
+
+   // Move selectedBusiness card to the front of the list
+   if (selectedBusiness) {
+      const selectedIndex = geoJson.features.findIndex((feature) => feature.properties.name === selectedBusiness);
+      const selectedCard = cards.splice(selectedIndex, 1)[0];
+      cards.unshift(selectedCard);
+   }
+
    return (
       <div className='aside'>
-         {selectedBusiness && <Card title={selectedBusiness.name} imageSrc={logo} caption={selectedBusiness.location.display_address} />}
-         {selectedBusiness && <Card title={selectedBusiness.name} imageSrc={logo} caption={selectedBusiness.location.display_address} />}
-         {selectedBusiness && <Card title={selectedBusiness.name} imageSrc={logo} caption={selectedBusiness.location.display_address} />}
-
-         {selectedBusiness && <Card title={selectedBusiness.name} imageSrc={logo} caption={selectedBusiness.location.display_address} />}
-
-         {selectedBusiness && <Card title={selectedBusiness.name} imageSrc={logo} caption={selectedBusiness.location.display_address} />}
-
-         {selectedBusiness && <Card title={selectedBusiness.name} imageSrc={logo} caption={selectedBusiness.location.display_address} />}
-
-         {selectedBusiness && <Card title={selectedBusiness.name} imageSrc={logo} caption={selectedBusiness.location.display_address} />}
+         <h1 className='aside__title'>Results</h1>
+         <ul className='aside__list'>{cards}</ul>
       </div>
    );
 }
--- src/components/Map/Map.jsx
+++
@@ -1,7 +0,0 @@
-import './Map.scss';
-
-function Map() {
-   return <div className='map'></div>;
-}
-
-export default Map;

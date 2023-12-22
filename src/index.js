50d75149b78bea4e5a42662d874902b3347fa9ae
Felipe Gonzalez <pipegonzalez@Felipe-MacBook-Pro.local>
2023-12-22 00:01:32 -0800

    changing dependencies to deploy with Render.com

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.render(
   <React.StrictMode>

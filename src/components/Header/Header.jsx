1908403b2ccc064d9f1314a6b0ca4cd985e030ac
Felipe Gonzalez <contraviento@gmail.com>
2024-03-27 21:26:53 -0700

    hidde login icon until fix bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,37 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { FaUserAlt } from 'react-icons/fa';
+// import { FaUserAlt } from 'react-icons/fa';
 import logo from '../../assets/images/logo.svg';
 import './Header.scss';
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
-import LoginForm from '../LoginForm/LoginForm';
+// import Dialog from '@mui/material/Dialog';
+// import DialogContent from '@mui/material/DialogContent';
+// import useMediaQuery from '@mui/material/useMediaQuery';
+// import { useTheme } from '@mui/material/styles';
+// import LoginForm from '../LoginForm/LoginForm';
 
-const styleBg = {
-	bgcolor: 'rgba(43, 40, 64, 0.2)',
-};
+// const styleBg = {
+// 	bgcolor: 'rgba(43, 40, 64, 0.2)',
+// };
 
-const styleBox = {
-	width: '500px',
-	height: '450px',
-	display: 'flex',
-	alignItems: 'center',
-};
+// const styleBox = {
+// 	width: '500px',
+// 	height: '450px',
+// 	display: 'flex',
+// 	alignItems: 'center',
+// };
 
 function Header({ handleSearchInput }) {
-	const [open, setOpen] = useState(false);
-	const theme = useTheme();
-	const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+	// const [open, setOpen] = useState(false);
+	// const theme = useTheme();
+	// const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
-
-	const handleClose = () => {
-		setOpen(false);
-	};
+	// const handleClickOpen = () => {
+	// 	setOpen(true);
+	// };
+	//
+	// 	const handleClose = () => {
+	// 		setOpen(false);
+	// 	};
 
 	return (
 		<header className='header-container'>
@@ -43,25 +43,15 @@ function Header({ handleSearchInput }) {
 			</div>
 			<div className='header-container__row'>
 				<div className='header-container__search'>
-					<input
-						type='text'
-						placeholder='Type business name...'
-						className='header-container__search-input'
-						onChange={handleSearchInput}
-					/>
+					<input type='text' placeholder='Type business name...' className='header-container__search-input' onChange={handleSearchInput} />
 					<button className='header-container__search-button'>
 						<FaSearch style={{ color: '#278C8C' }} />
 					</button>
 				</div>
-				<div onClick={handleClickOpen} className='header-container__avatar'>
+				{/* <div onClick={handleClickOpen} className='header-container__avatar'>
 					<FaUserAlt style={{ color: '#278C8C' }} />
-				</div>
+				</div> */}
 			</div>
-			<Dialog sx={styleBg} fullScreen={fullScreen} open={open} onClose={handleClose} aria-labelledby='responsive-dialog-title'>
-				<DialogContent sx={styleBox}>
-					<LoginForm />
-				</DialogContent>
-			</Dialog>
 		</header>
 	);
 }

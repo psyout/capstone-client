16d6dae0092c5b350ac1fa85b13ca29a920cd612
Felipe Gonzalez <contraviento@gmail.com>
2025-02-25 09:12:15 -0800

    separate some use of constant into of serach by into a new jsx file

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -7,37 +7,7 @@ import formatHours from './FormatHours';
 import formatDrinks from './FormatDrinks';
 import formatFood from './FormatFood';
 import Footer from '../Footer/Footer';
-
-const options = [
-	{ value: 'name', label: 'Name' },
-	{ value: 'hours', label: 'Time' },
-];
-
-const filters = [
-	{ value: '', label: 'Categories' },
-	{ value: 'Seafood', label: 'Seafood' },
-	{ value: 'Bars', label: 'Bars' },
-	{ value: 'Canadian', label: 'Canadian' },
-	{ value: 'Karaoke', label: 'Karaoke' },
-	{ value: 'Deli', label: 'Deli' },
-	{ value: 'Cideries', label: 'Cideries' },
-	{ value: 'Restaurant', label: 'Restaurant' },
-	{ value: 'Cocktail', label: 'Cocktail' },
-	{ value: 'Mexican', label: 'Mexican' },
-	{ value: 'Lounges', label: 'Lounges' },
-	{ value: 'Spanish', label: 'Spanish' },
-	{ value: 'Australian', label: 'Australian' },
-	{ value: 'Pub', label: 'Pub' },
-	{ value: 'Gastropub', label: 'Gastropub' },
-	{ value: 'Persian', label: 'Persian' },
-	{ value: 'Dive Bars', label: 'Dive Bars' },
-];
-
-const hoodFilter = [
-	{ value: '', label: 'Neighbourhood' },
-	{ value: 'Kitsilano', label: 'Kitsilano' },
-	{ value: 'Downtown', label: 'Downtown' },
-];
+import { options, filters, hoodFilter } from './SearchBy';
 
 const filterAndSort = (features, search, filterBy, hoodBy, sortBy, excludeColumns) => {
 	const lowerCasedSearch = search.toLowerCase();
---
+++ src/components/Aside/SearchBy.jsx
@@ -0,0 +1,30 @@
+export const options = [
+	{ value: 'name', label: 'Name' },
+	{ value: 'hours', label: 'Time' },
+];
+
+export const filters = [
+	{ value: '', label: 'Categories' },
+	{ value: 'Seafood', label: 'Seafood' },
+	{ value: 'Bars', label: 'Bars' },
+	{ value: 'Canadian', label: 'Canadian' },
+	{ value: 'Karaoke', label: 'Karaoke' },
+	{ value: 'Deli', label: 'Deli' },
+	{ value: 'Cideries', label: 'Cideries' },
+	{ value: 'Restaurant', label: 'Restaurant' },
+	{ value: 'Cocktail', label: 'Cocktail' },
+	{ value: 'Mexican', label: 'Mexican' },
+	{ value: 'Lounges', label: 'Lounges' },
+	{ value: 'Spanish', label: 'Spanish' },
+	{ value: 'Australian', label: 'Australian' },
+	{ value: 'Pub', label: 'Pub' },
+	{ value: 'Gastropub', label: 'Gastropub' },
+	{ value: 'Persian', label: 'Persian' },
+	{ value: 'Dive Bars', label: 'Dive Bars' },
+];
+
+export const hoodFilter = [
+	{ value: '', label: 'Neighbourhood' },
+	{ value: 'Kitsilano', label: 'Kitsilano' },
+	{ value: 'Downtown', label: 'Downtown' },
+];
